5913ee1ca6ab8701b7de7be8837d0ce50665a4ef
JosieRice <josiericetong@gmail.com>
2023-10-06 14:56:50 -0700

    module federation error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
+const NextFederationPlugin = require('@module-federation/nextjs-mf');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack(config, options) {
+        const { isServer } = options;
+        config.plugins.push(
+          new NextFederationPlugin({
+            name: 'components',
+            remotes: {
+              container: `container@http://localhost:3000/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
+            },
+            filename: 'static/chunks/remoteEntry.js',
+            exposes: {
+            //   './title': './components/exposedTitle.js',
+            //   './checkout': './pages/checkout',
+            },
+            shared: {
+              // whatever else
+            },
+          })
+        );
+    
+        return config;
+      },
+}
 
 module.exports = nextConfig
---
+++ src/components/Button.tsx
@@ -0,0 +1,3 @@
+export function Button() {
+  return <button>button from components app</button>;
+}
